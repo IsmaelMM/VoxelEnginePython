a2e6a586ee95ea6bd353c00b9a3b90153061d445
Ismael <leamsi78@gmail.com>
2024-01-08 11:57:24 +0100

    Texture Array

--- world_objects/chunk.py
+++ world_objects/chunk.py
@@ -50,7 +50,7 @@ class Chunk:
 
                 for y in range(local_height):
                     wy = y + cy
-                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = rng
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = 2  # 2 -> Texture array
 
         if np.any(voxels):
             self.is_empty = False
